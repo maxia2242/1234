e8c5e64fe9d862a237883e0d82695d7ab1950cb7
maxia2242 <maxia2242@HRH-IC0027346.wrdsb.ca>
2016-05-27 09:10:11 -0400

    Hello

---
+++ src/s/s/p/SSP.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s.s.p;
+
+/**
+ *
+ * @author maxia2242
+ */
+public class SSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
